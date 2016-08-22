9e446ec23a3858733b32f80ff0de34b7990e06f8
Ian Slacky <Ian Slacky>
2016-08-22 22:35:23 +0100

    zone 1 edit - Add logo/copyright to scene transitions

--- levels/planet1/zone1.lua
+++ levels/planet1/zone1.lua
@@ -24,7 +24,7 @@ local levelData = {
             {object="scenery", x=370, y=-260, type="fg-foilage-2-yellow", layer=2, size=0.5, flip="x"},
         
         {object="ledge", x=280},
-            {object="scenery", x=90, y=-300, type="fg-tree-4-yellow"},
+            {object="scenery", x=90, y=-300, size=1.1, type="fg-tree-4-yellow"},
             
         {object="ledge", x=280, y=-200},
             {object="scenery", x=0, y=-145, type="fg-flowers-3-yellow", layer=2, size=0.7, onLedge=true},
@@ -36,17 +36,22 @@ local levelData = {
         {object="ledge", x=200, y=-200},
             {object="rings", color=aqua, trajectory={x=50, y=-100, xforce=130, yforce=15, arc=40, num=3}},
             --pinacle of scenery
-            {object="scenery", x=360, y=-190, type="fg-tree-4-yellow"},
-            {object="scenery", x=200, y=220,  type="fg-wall-left"},
-            {object="scenery", x=300, y=220,  type="fg-wall-right"},
+            {object="scenery", x=300, y=-380, size=1.2, type="fg-tree-4-yellow"},
+            {object="scenery", x=200, y=245,  type="fg-wall-left"},
+            {object="scenery", x=300, y=245,  type="fg-wall-right"},
             -- top row of blocks
-            {object="scenery", x=0,   y=320, type="fg-wall-left"},
-            {object="scenery", x=100, y=320, type="fg-wall-middle"},
-            {object="scenery", x=480, y=320, type="fg-wall-right"},
+            {object="scenery", x=0,   y=345, type="fg-wall-left"},
+            {object="scenery", x=100, y=345, type="fg-wall-middle"},
+            {object="scenery", x=480, y=345, type="fg-wall-right"},
             -- bottom row of blocks
-            {object="scenery", x=-150, y=420, type="fg-wall-left"},
-            {object="scenery", x=-50,  y=420, type="fg-wall-middle", copy=2},
-            {object="scenery", x=710,  y=420, type="fg-wall-right"},
+            {object="scenery", x=-150, y=445, type="fg-wall-left"},
+            {object="scenery", x=-50,  y=445, type="fg-wall-middle", copy=2},
+            {object="scenery", x=710,  y=445, type="fg-wall-right"},
+
+            {object="scenery", x=-250, y=545, type="fg-wall-left"},
+            {object="scenery", x=-150, y=545, type="fg-wall-middle", copy=3},
+            {object="scenery", x=980,  y=545, type="fg-wall-right"},
+
 
         {object="ledge", x=300, y=-30, size="medium2"},
             {object="scenery", x=0, y=-175, type="fg-flowers-1-yellow", layer=2, onLedge=true},
